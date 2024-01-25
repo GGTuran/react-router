b163a41ebb880099afed0f1915b580d31f9a6111
GGTuran <tayyabturan67@gmail.com>
2024-01-25 17:46:56 +0600

    moja lagtese na

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,10 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
+import './users.css'
 
 
 const Users = () => {
+
+    const users = useLoaderData();
     return (
-        <div>
-           <h2>Meet our users</h2> 
+        <div >
+           <h2>Meet our users:{users.length} </h2> 
            <p>Jara mara khabe lol</p>
+           <div className="users">
+            {
+                users.map(user => <User key={user.id} user={user} ></User>)
+            }
+           </div>
         </div>
     );
 };
