93fb43ee767716fdf1f79899c9a5cacd85ce445e
GGTuran <tayyabturan67@gmail.com>
2024-01-26 10:28:24 +0600

    showed details

---
+++ src/Components/UserDetails/UserDetails.jsx
@@ -0,0 +1,10 @@
+
+const UserDetails = () => {
+    return (
+        <div>
+            <h1>Details of user</h1>
+        </div>
+    );
+};
+
+export default UserDetails;
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Home from './Components/Home/Home.jsx';
 import About from './Components/About/About.jsx';
 import Contact from './Components/Contact/Contact.jsx';
 import Users from './Components/Users/Users.jsx';
+import UserDetails from './Components/UserDetails/UserDetails.jsx';
 
 
 const router = createBrowserRouter([
@@ -30,6 +31,10 @@ const router = createBrowserRouter([
         path: "/users",
         loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
         element: <Users></Users>
+      },
+      {
+        path: '/user/:userId',
+        element: <UserDetails></UserDetails>
       }
     ]
   },
